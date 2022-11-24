7807c127dc57905daa0e1307ea3b452142cf3360
Surender Kumar <111198040+monudisodia@users.noreply.github.com>
2022-11-25 00:41:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,14 @@ import { useEffect , useState } from 'react';
 function App() {
 
   const [data, setData] = useState([]);
-
+  const getName =(async()=>{
+    const url =await fetch('https://jsonplaceholder.typicode.com/users')
+    const res = await url.json()
+    setData(res)
+  })
+  useEffect(()=>{
+    getName()
+  },[])
   /**
    * fetch data from api on mount. 
    */
